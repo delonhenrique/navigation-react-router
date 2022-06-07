c383891c306796631559af761dfca244a1bb6198
Delon Henrique <delon.henrique@gmail.com>
2022-06-07 00:11:46 -0300

    navigation-react-router

---
+++ src/views/examples/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = (props) => {
+    return (
+        <div className="About">
+            <h1>Sobre</h1>
+            <h3>Sobre o sistema...</h3>
+        </div>
+    );
+}
+
+export default About
---
+++ src/views/examples/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = (props) => {
+    return (
+        <div className="NotFound">
+            <h1>404 - Not Found</h1>
+            <h3>Página não encontrada</h3>
+        </div>
+    );
+}
+
+export default NotFound
